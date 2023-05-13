347d27bc19c9cc6a3f89072492e0b592fbc03436
Eiji Hirakawa <e.hirakawa@hotmail.co.jp>
2023-05-13 14:16:56 +0900

    Fixed Report search/count request properties

--- rtc4j/src/main/java/com/rapidtable/sdk/rtc4j/resource/report/ReportBulkCountRequest.java
+++ rtc4j/src/main/java/com/rapidtable/sdk/rtc4j/resource/report/ReportBulkCountRequest.java
@@ -61,6 +61,7 @@ public class ReportBulkCountRequest implements IRequest {
         private String gt = null;
         private String gte = null;
         private String term = null;
+        private List<String> ids = new ArrayList<>();
 
         public ReportBulkCountRequest build() throws TooManyRequestException {
             final var path = PathConfig.ROOT + PathConfig.WORKSPACE + String.format("/%s", workspaceId) +
@@ -109,6 +110,9 @@ public class ReportBulkCountRequest implements IRequest {
             }
             if (Objects.nonNull(term)) {
                 params.add("term=" + term);
+            }
+            if (Objects.nonNull(ids) && !ids.isEmpty()) {
+                params.add("ids=" + String.join(",", ids));
             }
 
             final var query = String.join("&", params);
@@ -245,6 +249,15 @@ public class ReportBulkCountRequest implements IRequest {
          */
         public Builder termTo(final String fieldId, final String to) {
             this.term = String.format("%s:,%s", fieldId, to);
+            return this;
+        }
+
+        /**
+         * Write the report IDs separated by commas.
+         * ids=report-id1,report-id2,report-id3
+         */
+        public Builder ids(final String... ids) {
+            this.ids.addAll(Arrays.asList(ids));
             return this;
         }
     }
--- rtc4j/src/main/java/com/rapidtable/sdk/rtc4j/resource/report/ReportBulkSearchRequest.java
+++ rtc4j/src/main/java/com/rapidtable/sdk/rtc4j/resource/report/ReportBulkSearchRequest.java
@@ -65,6 +65,7 @@ public class ReportBulkSearchRequest implements IRequest {
         private String gt = null;
         private String gte = null;
         private String term = null;
+        private List<String> ids = new ArrayList<>();
 
         public ReportBulkSearchRequest build() throws TooManyRequestException {
             final var path = PathConfig.ROOT + PathConfig.WORKSPACE + String.format("/%s", workspaceId) +
@@ -126,6 +127,9 @@ public class ReportBulkSearchRequest implements IRequest {
             }
             if (Objects.nonNull(term)) {
                 params.add("term=" + term);
+            }
+            if (Objects.nonNull(ids) && !ids.isEmpty()) {
+                params.add("ids=" + String.join(",", ids));
             }
 
             final var query = String.join("&", params);
@@ -300,6 +304,15 @@ public class ReportBulkSearchRequest implements IRequest {
          */
         public Builder termTo(final String fieldId, final String to) {
             this.term = String.format("%s:,%s", fieldId, to);
+            return this;
+        }
+
+        /**
+         * Write the report IDs separated by commas.
+         * ids=report-id1,report-id2,report-id3
+         */
+        public Builder ids(final String... ids) {
+            this.ids.addAll(Arrays.asList(ids));
             return this;
         }
     }
